02e4694b8a32747dfd716e34adf26f9252836ae3
Angelos <angtsalas@gmail.com>
2019-02-18 20:48:12 +0200

    Add Prettier and apply it to code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
-import store from './state/store'
-import './App.css';
+import React, { Component } from "react";
+import store from "./state/store";
+import "./App.css";
 import Container from "./components/Container";
-import {saveToLocal} from "./utils/utils";
-
+import { saveToLocal } from "./utils/utils";
 
 //Saves the state to local storage when app closes or reloads
-window.onbeforeunload = function(){
-	localStorage.clear();
-    saveToLocal(store.getState());
-}
+window.onbeforeunload = function() {
+  localStorage.clear();
+  saveToLocal(store.getState());
+};
 
 class App extends Component {
   render() {
-   return (
+    return (
       <div className="App">
-		<Container />
-	  </div>
+        <Container />
+      </div>
     );
   }
 }
 
 export default App;
-
